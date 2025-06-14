ee33f4c0359073de285ce4b5aa0c823a2f87bcc4
duarte77 <adrianoduartehpz@gmail.com>
2025-06-14 11:51:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -48,5 +48,5 @@ app.post('/connect-linkedin', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`🔥 Servidor rodando na porta ${PORT}`);
+  console.log(`🔥 Servidorr rodando na porta ${PORT}`);
 });
